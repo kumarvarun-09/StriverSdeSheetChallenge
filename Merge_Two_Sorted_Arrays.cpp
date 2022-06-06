4097bec7e7b8c4321d52b98b617b646b3a17504c
kumarvarun-09 <97241400+kumarvarun-09@users.noreply.github.com>
2022-06-06 14:09:04 +0530

    Update Merge_Two_Sorted_Arrays.cpp

--- Merge_Two_Sorted_Arrays.cpp
+++ Merge_Two_Sorted_Arrays.cpp
@@ -1,33 +1,28 @@
 #include <bits/stdc++.h> 
-vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+vector<int> ninjaAndSortedArrays(vector<int>& nums1, vector<int>& nums2, int m, int n) 
+{
 	// Write your code here.
-    vector<int> ans;
-    int i = 0, j = 0;
-    while(i < m || j < n)
-    {
-        if(i < m && j < n)
+       int s = m + n, i = m - 1, j = n - 1;
+        while(s--)
         {
-            if(arr1[i] < arr2[j])
-            {
-                ans.push_back(arr1[i]);
-                i++;
+            if(i >= 0 && j >= 0)
+            { 
+                if(nums1[i] > nums2[j])
+                {
+                    nums1[s] = nums1[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[s] = nums2[j];
+                    j--;
+                }
             }
-            else
+            else if(j >= 0)
             {
-                ans.push_back(arr2[j]);
-                j++;
-            }    
+                   nums1[s] = nums2[j];
+                   j--;
+            }
         }
-        else if(i < m)
-        {
-              ans.push_back(arr1[i]);
-              i++;
-        }
-         else if(j < n)
-        {
-              ans.push_back(arr2[j]);
-              j++;
-        }
-    }
-    return ans;
+    return nums1;
 }
