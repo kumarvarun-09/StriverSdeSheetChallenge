f444452f482223c54217ccbc447082f34b521e3c
kumarvarun-09 <97241400+kumarvarun-09@users.noreply.github.com>
2022-06-20 00:50:48 +0530

    Create Preorder_Traversal.cpp

---
+++ Preorder_Traversal.cpp
@@ -0,0 +1,27 @@
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+void traverse(TreeNode* root, vector<int> &ans)
+    {
+        if(!root)
+            return;
+        ans.push_back(root -> data);
+        traverse(root -> left, ans);
+        traverse(root -> right, ans);
+    }
+vector<int> getPreOrderTraversal(TreeNode *root)
+{
+    // Write your code here.
+    vector<int> ans;
+    traverse(root, ans);
+    return ans;
+}
