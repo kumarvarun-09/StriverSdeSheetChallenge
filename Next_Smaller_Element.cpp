845181d1c0a14331b90c5f0f3df70239f408c8cc
kumarvarun-09 <97241400+kumarvarun-09@users.noreply.github.com>
2022-07-03 19:22:40 +0530

    Create Next_Smaller_Element.cpp

---
+++ Next_Smaller_Element.cpp
@@ -0,0 +1,20 @@
+#include<stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+    stack<int> st;
+    vector<int> ans(n, -1);
+    for(int i = n - 1; i >= 0; i--)
+    {
+         while(st.size() && st.top() >= arr[i])
+         {
+                st.pop();
+         }
+         if(st.size() && st.top() < arr[i])
+         {
+               ans[i] = st.top();
+         }
+         st.push(arr[i]);
+    }
+     return ans;
+}
